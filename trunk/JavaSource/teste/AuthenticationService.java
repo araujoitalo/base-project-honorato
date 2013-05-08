b7f564da686b57bb082c830f553f87afd0bd408c
marcelohonorato@gmail.com <marcelohonorato@gmail.com@596883fb-8caf-b121-f38a-e9f7db720f73>
2013-05-08 06:39:27 +0000

    Reestruturação dos pacotes

---
+++ trunk/JavaSource/teste/AuthenticationService.java
@@ -0,0 +1,11 @@
+package teste;
+
+import javax.annotation.security.RolesAllowed;
+
+public interface AuthenticationService {
+	
+	boolean login(String username, String password);
+
+	@RolesAllowed({"ROLE_ADMIN","ROLE_REGISTERED"})
+	void logout();
+}
