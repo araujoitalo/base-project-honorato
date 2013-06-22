c7c1a48b95283499c9ea0052642565514a68450d
marcelohonorato@gmail.com <marcelohonorato@gmail.com@596883fb-8caf-b121-f38a-e9f7db720f73>
2013-06-22 18:46:14 +0000

    Inclusão de interceptadores e log

---
+++ JavaSource/samples/CustomerService.java
@@ -0,0 +1,25 @@
+package samples;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+ 
+@Service
+@Scope("prototype")
+public class CustomerService 
+{
+	String message;
+ 
+	public String getMessage() {
+		return message;
+	}
+ 
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ZZZ')")
+	public void teste(){
+		System.out.println("TETETTE");
+	}
+}
