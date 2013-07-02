5475ed2faab96431192cba12b3ce76f7ea1034c6
marcelohonorato@gmail.com <marcelohonorato@gmail.com@596883fb-8caf-b121-f38a-e9f7db720f73>
2013-07-02 23:37:59 +0000

    Teste de uso de tree

--- trunk/JavaSource/br/com/honorato/dao/implement/ResourceDAO.java
+++ trunk/JavaSource/br/com/honorato/dao/implement/ResourceDAO.java
@@ -4,21 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
-import br.com.honorato.dao.entity.Module;
+import br.com.honorato.dao.entity.Resource;
 
-public class ModuleDAO extends JpaDAO<Module> {
+public class ResourceDAO extends JpaDAO<Resource> {
 
 	private static final long serialVersionUID = 1291101069692295470L;
 	
-	public ModuleDAO(EntityManager manager){
+	public ResourceDAO(EntityManager manager){
 		super(manager);
 	}
 	
-	public Module getFullTree(String rootCode){
+	public Resource getFullTree(String rootCode){
 		
-		Module result;
+		Resource result;
 
-		TypedQuery<Module> consult = this.getEntityManager().createQuery("from Module m where m.code = :rootCode", this.persistentClass);
+		TypedQuery<Resource> consult = this.getEntityManager().createQuery("from Module m where m.code = :rootCode", this.persistentClass);
 		consult.setParameter("rootCode", rootCode);
 
 		try {
