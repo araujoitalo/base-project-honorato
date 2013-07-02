032aada10b4b3e1aaee1a2a2231d3389c70eb774
marcelohonorato@gmail.com <marcelohonorato@gmail.com@596883fb-8caf-b121-f38a-e9f7db720f73>
2013-07-02 20:18:58 +0000

    Teste de uso de tree

---
+++ JavaSource/br/com/honorato/view/managedbean/TreeBean.java
@@ -0,0 +1,59 @@
+package br.com.honorato.view.managedbean;
+
+import java.io.Serializable;  
+import javax.faces.application.FacesMessage;  
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;  
+import javax.faces.event.ActionEvent;  
+  
+import org.primefaces.model.DefaultTreeNode;  
+import org.primefaces.model.TreeNode;  
+  
+@ManagedBean(name = "treeBean")
+@ViewScoped
+public class TreeBean implements Serializable {  
+  
+    private TreeNode root;  
+  
+    private TreeNode selectedNode;  
+  
+    public TreeBean() {  
+        root = new DefaultTreeNode("Root", null);  
+        TreeNode node0 = new DefaultTreeNode("Node 0", root);  
+        TreeNode node1 = new DefaultTreeNode("Node 1", root);  
+        TreeNode node2 = new DefaultTreeNode("Node 2", root);  
+  
+        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);  
+        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);  
+  
+        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);  
+        TreeNode node11 = new DefaultTreeNode("Node 1.1", node1);  
+  
+        TreeNode node000 = new DefaultTreeNode("Node 0.0.0", node00);  
+        TreeNode node001 = new DefaultTreeNode("Node 0.0.1", node00);  
+        TreeNode node010 = new DefaultTreeNode("Node 0.1.0", node01);  
+  
+        TreeNode node100 = new DefaultTreeNode("Node 1.0.0", node10);  
+    }  
+  
+    public TreeNode getRoot() {  
+        return root;  
+    }  
+  
+    public TreeNode getSelectedNode() {  
+        return selectedNode;  
+    }  
+  
+    public void setSelectedNode(TreeNode selectedNode) {  
+        this.selectedNode = selectedNode;  
+    }  
+      
+    public void displaySelectedSingle(ActionEvent event) {  
+        if(selectedNode != null) {  
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());  
+  
+            FacesContext.getCurrentInstance().addMessage(null, message);  
+        }  
+    }  
+}
